a315bf1d0527177f373235cc4bbf7f1796ee7ae6
milton cañari <devmeams>
2023-09-21 07:15:05 -0500

    custom hook

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { CounterApp } from './useState/CounterApp'
+//import { CounterApp } from './useState/CounterApp'
+import { CounterWithCustomHook } from './useState/CounterWithCustomHook'
 //import { HooksApp } from './HooksApp.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CounterApp />
+    <CounterWithCustomHook />
   </React.StrictMode>,
 )
--- src/useState/CounterWithCustomHook.jsx
+++ src/useState/CounterWithCustomHook.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from "react";
+import { useCounter } from "../hooks/useCounter";
 
 export const CounterWithCustomHook = () => {
+  const { counter, incremento,decrement,reset} = useCounter();
   return (
-    <div>CounterWithCustomHook</div>
-  )
-}
+    <div className="container">
+      <h1>CounterWithCustomHook : {counter}</h1>
+      <hr />
+      <button className="btn btn-outline-primary" onClick={()=>incremento(2)}>+1</button>
+      <button className="btn btn-outline-primary" onClick={reset}>reset</button>
+      <button className="btn btn-outline-primary" onClick={()=>decrement(2)}>-1</button>
+    </div>
+  );
+};
